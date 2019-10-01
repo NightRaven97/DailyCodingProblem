b771eccd9e842d3358c32054231a99c50dfc63d5
Nandan Satheesh <nandan.199776@gmail.com>
2019-10-01 17:08:43 +0530

    Major Changes

--- challenges/largest-non-adjacent-sum.py
+++ challenges/largest-non-adjacent-sum.py
@@ -19,27 +19,35 @@ import math
 
 def maximumNonAdjacentSum(array):
     
-    DP = [0]*len(array)
+    if (len(array) == 1):
+        return array[0]
     
+    if (len(array) == 2):
+        return max(array[0] , array[1])
+    
+    DP = [0]*len(array)
     DP[0] = array[0]
     DP[1] = max(array[0] , array[1])
     
     for i in range(2,len(array)):
     	
-        DP[i] = max3( array[i] , array[i] + DP[i-2] , DP[i-1])
+        DP[i] = max( array[i] , array[i] + DP[i-2] , DP[i-1])
         print(DP)
-        
-    
-    
-    
+
     return DP[len(array) - 1]
 
-maximumNonAdjacentSum([2,4,6,2,5])
+# Outputs 
+maximumNonAdjacentSum([2])
+# 2
 
-# Outputs
+maximumNonAdjacentSum([2,3])
+# 3
+
+print(maximumNonAdjacentSum([2,4,6,2,5]))
 # [2, 4, 8, 0, 0]
 # [2, 4, 8, 8, 0]
 # [2, 4, 8, 8, 13]
+# 13
 
 print(maximumNonAdjacentSum([5,1,1,5]))
 # [5, 5, 6, 0]
